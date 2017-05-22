1ce53bdf6f0adb0198ecb3046fdada00474ab7f4
KimJuYoung93 <kjy@git.co.kr>
2017-05-22 16:00:31 +0900

    commit

---
+++ GitSampleProject/src/main/java/com/git/sample/Request.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.git.sample;
+
+/** 
+* @ClassName: Request 
+* @Description: 
+* @author JY.Kim 
+* @date 2017. 5. 22. 오후 4:00:02 
+*  
+*/
+public class Request {
+	public void name(){
+		System.out.println("요청");
+	}
+}
