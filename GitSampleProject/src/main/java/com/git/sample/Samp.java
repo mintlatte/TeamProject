6c0a2a8ce7271abad7a2cde2e5d6b49604988018
KimJuYoung93 <kjy@git.co.kr>
2017-05-22 14:04:46 +0900

    정배씨 커밋

---
+++ GitSampleProject/src/main/java/com/git/sample/Samp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.git.sample;
+
+/** 
+* @ClassName: Samp 
+* @Description: 
+* @author JY.Kim 
+* @date 2017. 5. 22. 오후 2:03:59 
+*  
+*/
+public class Samp {
+	
+	public void sample(){
+		System.out.println("정배씨 깃허브");
+	}
+
+}
